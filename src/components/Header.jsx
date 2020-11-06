f71e13d9bbc52bc933d7057b33c0f494e56b6d00
RYOTA <kobaryo04ysh@icloud.com>
2020-11-06 18:24:29 +0900

    modify: header profile link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Container, Navbar } from "react-bootstrap"
-import { Link } from 'react-router-dom'
+import { Link, useHistory } from 'react-router-dom'
+
 
 //material ui
 import Button from '@material-ui/core/Button';
@@ -16,15 +17,23 @@ const useStyles = makeStyles((theme) => ({
 
 export default function Header() {
     const classes = useStyles();
+    const history = useHistory()
+    
 
     return (
         <Container style={{ margin: "0", padding: "0", minWidth: "100vw" }} >
             <Navbar expand="lg" variant="light" bg="light" fixed="top" style={{ justifyContent: "space-between", padding: "1% 8%" }}>
                 <Navbar.Brand href="/" style={{ fontSize: "1.5rem" }}>Start</Navbar.Brand>
                 <Link to="/dashboard" className={classes.headerRightButton}>
-                    <Button ize="large" color="primary" variant="contained">
-                        プロフィールを表示
-                    </Button>
+                {
+                    history.location.pathname === '/dashboard' ? (
+                        <Button></Button> 
+                        ) : ( 
+                        <Button size="large" color="primary" variant="contained">
+                            プロフィールを表示
+                        </Button>
+                        )
+                }
                 </Link>
             </Navbar>
         </Container>
