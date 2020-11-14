acb2be56e423c29be5a66868ac910f4b37d7f81c
RYOTA <kobaryo04ysh@icloud.com>
2020-11-14 11:46:57 +0900

    dev: add menu to more vert menu

--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -5,13 +5,14 @@ import { Link } from 'react-router-dom'
 //materialUI
 import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
-import CardHeader from '@material-ui/core/CardHeader';
 import CardContent from "@material-ui/core/CardContent";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import { db } from '../firebase'
 import IconButton from '@material-ui/core/IconButton';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
 
 const useStyles = makeStyles({
     backLink: {
@@ -55,12 +56,33 @@ const useStyles = makeStyles({
     }
 });
 
+const options = [
+  '編集',
+  '削除'
+];
+
+//パフォーマンステスト用
+// if (process.env.NODE_ENV !== 'production') {
+//     const {whyDidYouUpdate} = require('why-did-you-update');
+//     whyDidYouUpdate(React);
+// }
+
 export default function Detail() {
     const classes = useStyles();
     const path = window.location.href;
     const id = path.split('/detail/')[1];
-
     const [postDetail, setPostDetail] = useState([])
+    
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const open = Boolean(anchorEl);
+  
+    const handleClick = (event) => {
+      setAnchorEl(event.currentTarget);
+    };
+  
+    const handleClose = () => {
+      setAnchorEl(null);
+    };
 
     useEffect(() => {
         getPost();
@@ -100,10 +122,27 @@ export default function Detail() {
             {postDetail.map(post => 
             <Card className={classes.root} key={post.id}>
                 <CardContent>
-                    <IconButton className={classes.threeDots}>
+                    <IconButton className={classes.threeDots} aria-haspopup="true" onClick={handleClick}>
                         <MoreVertIcon />
                     </IconButton>
-                    
+                    <Menu
+                        id="long-menu"
+                        anchorEl={anchorEl}
+                        keepMounted
+                        open={open}
+                        onClose={handleClose}
+                        PaperProps={{
+                        style: {
+                            width: '20ch',
+                        },
+                        }}
+                    >
+                        {options.map((option) => (
+                        <MenuItem key={option} onClick={handleClose}>
+                            {option}
+                        </MenuItem>
+                        ))}
+                    </Menu>
                     <Typography
                     className={classes.title}
                     color="textSecondary"
