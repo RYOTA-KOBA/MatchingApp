2af241c2ff68eba07af1ba9e3eea0043a186fa33
RYOTA <kobaryo04ysh@icloud.com>
2020-11-26 12:24:48 +0900

    add: test code

--- sample.test.js
+++ sample.test.js
@@ -3,3 +3,8 @@ describe("Jest動作チェック", () => {
         expect(3 + 7).toBe(10);
     });
 });
+describe("Jest動作チェック", () => {
+    test("サンプルテスト", async () => {
+        expect(5 + 5).toBe(10);
+    });
+});
