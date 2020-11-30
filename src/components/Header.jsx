4946398df83a2b2beb8e377420921b65a009f01e
RYOTA <kobaryo04ysh@icloud.com>
2020-11-30 20:48:39 +0900

    add Sample component for test

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,21 @@
-import React from 'react'
-import { Navbar } from "react-bootstrap"
-import HeaderRight from './HeaderRight'
-import { useAuth } from "../contexts/AuthContext"
+import React from "react";
+import { Navbar } from "react-bootstrap";
+import HeaderRight from "./HeaderRight";
+import { useAuth } from "../contexts/AuthContext";
+import Sample from "../../sample";
 
 export default function Header() {
-    const { currentUser } = useAuth()
+  const { currentUser } = useAuth();
 
-    return (
-        <div style={{ margin: "0", padding: "0", minWidth: "100vw" }} >
-            <Navbar expand="lg" fixed="top" className="header-nav">
-                <Navbar.Brand href="/" style={{ fontSize: "1.5rem", color: "white" }}>Start</Navbar.Brand>
-                { currentUser && <HeaderRight /> }
-            </Navbar>
-        </div>
-    )
+  return (
+    <div style={{ margin: "0", padding: "0", minWidth: "100vw" }}>
+      <Sample />
+      <Navbar expand="lg" fixed="top" className="header-nav">
+        <Navbar.Brand href="/" style={{ fontSize: "1.5rem", color: "white" }}>
+          Start
+        </Navbar.Brand>
+        {currentUser && <HeaderRight />}
+      </Navbar>
+    </div>
+  );
 }
