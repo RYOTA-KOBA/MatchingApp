fc8a6c8f27a65338ff920f6a35655fe77eb568c2
RYOTA <kobaryo04ysh@icloud.com>
2020-12-01 22:38:28 +0900

    modify: set defaultValue

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -1,137 +1,175 @@
-import React, { useContext, useState, useEffect } from "react"
-import { auth, db, timestamp } from "../firebase"
-import firebase from "../firebase"
-import { useHistory, Redirect } from "react-router-dom"
+import React, { useContext, useState, useEffect } from "react";
+import { auth, db, timestamp } from "../firebase";
+import firebase from "../firebase";
+import { useHistory, Redirect } from "react-router-dom";
 
-const AuthContext = React.createContext()
+const AuthContext = React.createContext();
 
 export function useAuth() {
-  return useContext(AuthContext)
+  return useContext(AuthContext);
 }
 
 export function AuthProvider({ children }) {
-  const [currentUser, setCurrentUser] = useState()
-  const [currentPost, setCurrentPost] = useState([])
-  const [currentId, setCurrentId] = useState()
-  const [loading, setLoading] = useState(true)
-  const history = useHistory()
+  const [currentUser, setCurrentUser] = useState();
+  const [currentPost, setCurrentPost] = useState([]);
+  const [currentId, setCurrentId] = useState();
+  const [defUsername, setDefUsername] = useState("");
+  const [loading, setLoading] = useState(true);
+  const history = useHistory();
 
   async function signup(username, email, password) {
-    const result = await auth.createUserWithEmailAndPassword(email, password)
-    console.log(result)
-    const user = result.user
+    const result = await auth.createUserWithEmailAndPassword(email, password);
+    console.log(result);
+    const user = result.user;
     if (user) {
-      const uid = user.uid
+      const uid = user.uid;
       const userInitialData = {
         email: email,
         uid: uid,
-        username: username
-      }
+        username: username,
+      };
 
-      db.collection('users').doc(uid).set(userInitialData)
-        .then('ユーザーが作成されました!')
+      db.collection("users")
+        .doc(uid)
+        .set(userInitialData)
+        .then("ユーザーが作成されました!");
     }
   }
 
   function login(email, password) {
-    return auth.signInWithEmailAndPassword(email, password)
+    return auth.signInWithEmailAndPassword(email, password);
   }
 
   function logout() {
-    return auth.signOut()
+    return auth.signOut();
   }
 
   function resetPassword(email) {
-    return auth.sendPasswordResetEmail(email)
+    return auth.sendPasswordResetEmail(email);
   }
 
-
   function updateUser(username, email, data) {
-    const uid = data.uid
-    return db.collection('users').doc(uid).set({
-      email: email,
-      username: username,
-    }, { merge: true })
-    .then(() => console.log("success!!"))
-}
-
+    const uid = data.uid;
+    return db
+      .collection("users")
+      .doc(uid)
+      .set(
+        {
+          email: email,
+          username: username,
+        },
+        { merge: true }
+      )
+      .then(() => console.log("success!!"));
+  }
 
   function updatePassword(password) {
-    return firebase.auth().currentUser.updatePassword(password)
+    return firebase.auth().currentUser.updatePassword(password);
   }
 
   const createPost = (title, content, authorName, uid) => {
     // const enterToBr = content.replace(/\r?\n/g, '<br>');
     // const brToBreak = enterToBr.replace(/(<br>|<br \/>)/gi, '\r\n');
-    db.collection('posts').add({
-      title: title,
-      content: content,
-      authorName: authorName,
-      createdAt: timestamp,
-      uid: uid,
-    })
-    .then(result => {
-      const id = result.id
-      console.log(result.id)
-      db.collection('posts').doc(id).set({id}, {merge: true})
-      .then(() => {
-        console.log("投稿成功！！")
-        history.push("/")
+    db.collection("posts")
+      .add({
+        title: title,
+        content: content,
+        authorName: authorName,
+        createdAt: timestamp,
+        uid: uid,
       })
-    })
-  }
+      .then((result) => {
+        const id = result.id;
+        console.log(result.id);
+        db.collection("posts")
+          .doc(id)
+          .set({ id }, { merge: true })
+          .then(() => {
+            console.log("投稿成功！！");
+            history.push("/");
+          });
+      });
+  };
 
-  const setNowId = id => {
-    return setCurrentId(id)
-  }
+  const setNowId = (id) => {
+    return setCurrentId(id);
+  };
 
-  const setNowPost = async(id) => {
+  const setNowPost = async (id) => {
     let post = [];
-    await db.collection('posts').doc(id).get()
-    .then(snapshot => {
-      const data = snapshot.data()
-      post.push({
-        title: data.title,
-        content: data.content
-      })
-    })
-    setCurrentPost(post[0])
-  }
+    await db
+      .collection("posts")
+      .doc(id)
+      .get()
+      .then((snapshot) => {
+        const data = snapshot.data();
+        post.push({
+          title: data.title,
+          content: data.content,
+        });
+      });
+    setCurrentPost(post[0]);
+  };
 
   const editPost = (title, content, data) => {
-    const id = data.id
-    db.collection('posts').doc(id).set({
-      title: title,
-      content: content
-    }, { merge: true })
-    .then(() => {
-      history.push('/')
-      console.log("更新成功!")
-    })
-  }
+    const id = data.id;
+    db.collection("posts")
+      .doc(id)
+      .set(
+        {
+          title: title,
+          content: content,
+        },
+        { merge: true }
+      )
+      .then(() => {
+        history.push("/");
+        console.log("更新成功!");
+      });
+  };
 
-  const savePostToBookmark = async(savedPosts) => {
-    const uid = currentUser.uid
-    const saveRef = db.collection('users').doc(uid).collection('bookmarks').doc();
-    savedPosts['saveId'] = saveRef.id;
+  const savePostToBookmark = async (savedPosts) => {
+    const uid = currentUser.uid;
+    const saveRef = db
+      .collection("users")
+      .doc(uid)
+      .collection("bookmarks")
+      .doc();
+    savedPosts["saveId"] = saveRef.id;
     await saveRef.set(savedPosts);
-  }
+  };
 
   const removePostFromBookmark = async (id) => {
-    const uid = currentUser.uid
-    await db.collection('users').doc(uid)
-        .collection('bookmarks').doc(id)
-        .delete();
+    const uid = currentUser.uid;
+    await db
+      .collection("users")
+      .doc(uid)
+      .collection("bookmarks")
+      .doc(id)
+      .delete();
+  };
+
+  const getCurrentUserName = (uid) => {
+    db.collection("users")
+      .doc(uid)
+      .get()
+      .then((snapshot) => {
+        const data = snapshot.data();
+        if (data.username !== "") {
+          setDefUsername(data.username);
+        }
+      });
   };
 
   useEffect(() => {
-    const unsubscribe = auth.onAuthStateChanged(user => {
-      setCurrentUser(user)
-      setLoading(false)
-      history.push("/")
-    })
-    return unsubscribe
-  }, [])
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      setCurrentUser(user);
+      setLoading(false);
+      if (user !== null) getCurrentUserName(user.uid);
+      history.push("/");
+    });
+    return unsubscribe;
+  }, []);
 
   const value = {
     currentUser,
@@ -149,14 +187,15 @@ export function AuthProvider({ children }) {
     setNowPost,
     currentPost,
     savePostToBookmark,
-    removePostFromBookmark
-  }
+    removePostFromBookmark,
+    defUsername,
+  };
 
   return (
     <AuthContext.Provider value={value}>
       {!loading && children}
     </AuthContext.Provider>
-  )
+  );
 }
 
 //コンポーネントが表示されなくなったら、loadingの!を外してログイン→!つけて再度ログイン
