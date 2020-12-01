2b77e4c9b94b5a25def166feb25d9b934670df4b
RYOTA <kobaryo04ysh@icloud.com>
2020-12-01 18:40:40 +0900

    modify: form style

--- src/components/UpdateProfile.jsx
+++ src/components/UpdateProfile.jsx
@@ -1,26 +1,37 @@
-import React, { useRef, useState } from "react";
-import { Form, Button, Card, Alert } from "react-bootstrap";
+import React, { useRef, useState, useCallback, useEffect } from "react";
+// import { Form, Button, Card, Alert } from "react-bootstrap";
 import { useAuth } from "../contexts/AuthContext";
 import { Link, useHistory } from "react-router-dom";
 import { db } from "../firebase";
 
-//material ui
-// import { makeStyles } from '@material-ui/core/styles';
-// import TextField from '@material-ui/core/TextField';
-// import Button from "@material-ui/core/Button";
-// import Card from '@material-ui/core/Card';
-// import CardContent from '@material-ui/core/CardContent';
+// material ui
+import { makeStyles } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import Alert from "@material-ui/lab/Alert";
 
-// const useStyles = makeStyles((theme) => ({
-//   root: {
-//     '& > *': {
-//       margin: theme.spacing(1),
-//       width: '95%',
-//     },
-//   },
-// }));
+const useStyles = makeStyles((theme) => ({
+  root: {
+    margin: "0 auto",
+    width: "500px",
+    display: "flex",
+    justifyContent: "center",
+  },
+  card: {
+    width: "600px",
+    padding: "30px",
+    paddingBottom: "0",
+    marginTop: "130px",
+  },
+  postFormTextField: {
+    width: "100%",
+    marginBottom: "15px",
+  },
+}));
 
-export default function UpdateProfile() {
+export default function UpdateProfile({ userName }) {
   const usernameRef = useRef();
   const emailRef = useRef();
   const passwordRef = useRef();
@@ -29,16 +40,49 @@ export default function UpdateProfile() {
   const [error, setError] = useState("");
   const [loading, setLoading] = useState(false);
   const history = useHistory();
-  // const classes = useStyles();
+  const classes = useStyles();
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordConfirm, setPasswordConfirm] = useState("");
+  const [defUsername, setDefUsername] = useState("");
+
+  const inputUsername = useCallback(
+    (event) => {
+      setUsername(event.target.value);
+    },
+    [setUsername]
+  );
+
+  const inputEmail = useCallback(
+    (event) => {
+      setEmail(event.target.value);
+    },
+    [setEmail]
+  );
+
+  const inputPassword = useCallback(
+    (event) => {
+      setPassword(event.target.value);
+    },
+    [setPassword]
+  );
+
+  const inputPasswordConfirm = useCallback(
+    (event) => {
+      setPasswordConfirm(event.target.value);
+    },
+    [setPasswordConfirm]
+  );
 
   function handleSubmit(e) {
     e.preventDefault();
-    if (passwordRef.current.value !== passwordConfirmRef.current.value) {
+    if (password !== passwordConfirm) {
       return setError("パスワードが一致しません");
     }
 
-    if (passwordRef.current.value) {
-      updatePassword(passwordRef.current.value);
+    if (password !== "") {
+      updatePassword(password);
     }
 
     const uid = currentUser.uid;
@@ -49,11 +93,7 @@ export default function UpdateProfile() {
         const data = snapshot.data();
         setLoading(true);
         setError("");
-        return updateUser(
-          usernameRef.current.value,
-          emailRef.current.value,
-          data
-        );
+        return updateUser(username, email, data);
       })
       .then(() => {
         // Success
@@ -78,52 +118,84 @@ export default function UpdateProfile() {
     }
   }
 
+  useEffect(() => {
+    const uid = currentUser.uid;
+    db.collection("users")
+      .doc(uid)
+      .get()
+      .then((snapshot) => {
+        const data = snapshot.data();
+        if (data.username !== "") {
+          setDefUsername(data.username);
+          console.log(defUsername);
+        }
+      });
+  }, []);
+
   return (
     <>
-      <Card>
-        <Card.Body>
-          <h2 className="text-center mb-4">プロフィールの編集</h2>
+      {error && (
+        <Alert className={classes.alert} severity="error">
+          {error}
+        </Alert>
+      )}
+      <Card className={classes.card}>
+        <CardContent>
+          <h2 className="text-center mb-4 update-profile-header">
+            プロフィールの編集 ⚙️
+          </h2>
           {error && <Alert variant="danger">{error}</Alert>}
-          <Form onSubmit={handleSubmit}>
-            <Form.Group id="username">
-              <Form.Label>名前</Form.Label>
-              <Form.Control
-                type="text"
-                ref={usernameRef}
-                required
-                defaultValue={currentUser.username}
-              />
-            </Form.Group>
-            <Form.Group id="email">
-              <Form.Label>Email</Form.Label>
-              <Form.Control
-                type="email"
-                ref={emailRef}
-                required
-                defaultValue={currentUser.email}
-              />
-            </Form.Group>
-            <Form.Group id="password">
-              <Form.Label>パスワード</Form.Label>
-              <Form.Control
-                type="password"
-                ref={passwordRef}
-                placeholder="空欄の場合は変更しません"
-              />
-            </Form.Group>
-            <Form.Group id="password-confirm">
-              <Form.Label>パスワードの編集</Form.Label>
-              <Form.Control
-                type="password"
-                ref={passwordConfirmRef}
-                placeholder="空欄の場合は変更しません"
-              />
-            </Form.Group>
-            <Button disabled={loading} className="w-100" type="submit">
+          <form onSubmit={handleSubmit}>
+            <TextField
+              className={classes.postFormTextField}
+              type="text"
+              ref={usernameRef}
+              label="名前"
+              required
+              value={username}
+              defaultValue={defUsername}
+              onChange={inputUsername}
+            />
+            <br />
+            <TextField
+              className={classes.postFormTextField}
+              type="email"
+              ref={emailRef}
+              label="Email"
+              required
+              defaultValue={currentUser.email}
+              onChange={inputEmail}
+            />
+            <br />
+            <TextField
+              className={classes.postFormTextField}
+              type="password"
+              ref={passwordRef}
+              label="パスワード"
+              placeholder="空欄の場合は変更しません"
+              onChange={inputPassword}
+            />
+            <br />
+            <TextField
+              className={classes.postFormTextField}
+              type="password"
+              ref={passwordConfirmRef}
+              label="パスワードの編集"
+              placeholder="空欄の場合は変更しません"
+              onChange={inputPasswordConfirm}
+            />
+            <br />
+            <Button
+              disabled={loading}
+              color="primary"
+              variant="contained"
+              className="w-100"
+              type="submit"
+            >
               編集を完了する
             </Button>
-          </Form>
-        </Card.Body>
+          </form>
+        </CardContent>
       </Card>
       <div className="w-100 text-center mt-2">
         <Link to="/">キャンセル</Link>
