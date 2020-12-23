42422b55df04b8c2a6d4a8de223a1402012ce333
RYOTA <kobaryo04ysh@icloud.com>
2020-12-23 22:56:48 +0900

    feat: add FollowButton component #147

---
+++ src/components/FollowButton.tsx
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import "../";
+
+// material ui
+import Button from "@material-ui/core/Button";
+
+export default function FollowButton() {
+  const [following, setFollowing] = useState(false);
+
+  // 確認用(後で消す)
+  const handleClick = () => {
+    setFollowing(!following);
+  };
+
+  return (
+    <div className="follow_btn-wrapper">
+      <Button
+        className={
+          "follow_btn-same" + " " + (following ? "following_btn" : "follow_btn")
+        }
+        variant="outlined"
+        onClick={handleClick}
+      >
+        {following ? "FOLLOWING" : "FOLLOW"}
+      </Button>
+    </div>
+  );
+}
