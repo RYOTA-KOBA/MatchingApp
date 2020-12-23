b343df38c782ac8b430dc9bc47b171acf3e4b204
RYOTA <kobaryo04ysh@icloud.com>
2020-12-23 20:15:14 +0900

    style: add Material Icon

--- src/components/TimeLineLink.tsx
+++ src/components/TimeLineLink.tsx
@@ -6,6 +6,8 @@ import { createStyles, Theme, makeStyles } from "@material-ui/core/styles";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
+import RssFeedIcon from "@material-ui/icons/RssFeed";
+import { green } from "@material-ui/core/colors";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -13,6 +15,11 @@ const useStyles = makeStyles((theme: Theme) =>
       maxWidth: 270,
       backgroundColor: theme.palette.background.paper,
       borderRadius: "12px",
+      marginTop: "20px",
+    },
+    feedicon: {
+      marginRight: "5px",
+      color: green[300],
     },
   })
 );
@@ -29,6 +36,7 @@ export default function TimeLineLink() {
     <>
       <List className={classes.root} aria-label="contacts">
         <ListItem button onClick={() => isSelected("timeline")}>
+          <RssFeedIcon className={classes.feedicon} />
           <ListItemText primary="タイムライン" id="timeline" />
         </ListItem>
       </List>
