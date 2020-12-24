078c22ac2ed08a90f22f4115aa726def8a36373c
RYOTA <kobaryo04ysh@icloud.com>
2020-12-24 11:30:50 +0900

    feat: Changed to not place the follow button on my profile #147

--- src/components/FollowButton.tsx
+++ src/components/FollowButton.tsx
@@ -1,27 +1,33 @@
 import React, { useState } from "react";
+import { useAuth } from "../contexts/AuthContext";
 
 // material ui
 import Button from "@material-ui/core/Button";
 
-export default function FollowButton() {
+export default function FollowButton({ uid }: any) {
+  const { currentUser }: any = useAuth();
   const [following, setFollowing] = useState(false);
 
-  // 確認用(後で消す)
+  // 確認用(後で消す!)
   const handleClick = () => {
     setFollowing(!following);
   };
 
   return (
     <div className="follow_btn-wrapper">
-      <Button
-        className={
-          "follow_btn-same" + " " + (following ? "following_btn" : "follow_btn")
-        }
-        variant="outlined"
-        onClick={handleClick}
-      >
-        {following ? "FOLLOWING" : "FOLLOW"}
-      </Button>
+      {currentUser.uid !== uid && (
+        <Button
+          className={
+            "follow_btn-same" +
+            " " +
+            (following ? "following_btn" : "follow_btn")
+          }
+          variant="outlined"
+          onClick={handleClick}
+        >
+          {following ? "FOLLOWING" : "FOLLOW"}
+        </Button>
+      )}
     </div>
   );
 }
